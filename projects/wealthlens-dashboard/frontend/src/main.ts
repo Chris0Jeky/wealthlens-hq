ab48bf638b8a3e02e8d6a325df9f951eb9b5b295
Chris0Jeky <jeky.tck@gmail.com>
2026-05-15 20:24:32 +0100

    refactor: extract Vue router to src/router/index.ts

--- projects/wealthlens-dashboard/frontend/src/main.ts
+++ projects/wealthlens-dashboard/frontend/src/main.ts
@@ -1,24 +1,8 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
+import router from "@/router";
 import "./style.css";
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: () => import("./views/HomeView.vue"),
-    },
-    {
-      path: "/charts/:name",
-      name: "chart",
-      component: () => import("./views/ChartView.vue"),
-    },
-  ],
-});
 
 const app = createApp(App);
 app.use(createPinia());
